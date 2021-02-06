55cd7a683b92366f19152fa0bdabfb2870d965d6
Robert Middleton <robert.middleton@rm5248.com>
2021-02-05 21:34:30 -0500

    Updated recv signal example

--- dbus_c++_receive_signal/recv_sig.cpp
+++ dbus_c++_receive_signal/recv_sig.cpp
@@ -3,10 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 
-using namespace std;
-
 //This is our signal handling method.
-void getSignal(int a, string str, int b){
+void getSignal(int a, std::string str, int b){
 	char* cstr = (char*)str.c_str();
 	
 	if( a < strlen(cstr) ){
@@ -22,26 +20,29 @@ void getSignal(int a, string str, int b){
 
 int main(int argc, char** argv){
 
-	//First we have to init the dbus.
-	DBus::init();
 	//The dispatcher sends us information.  Make sure that it doesn't go out of scope or bad things will happen.
-	DBus::Dispatcher::pointer dispatcher = DBus::Dispatcher::create();
+	std::shared_ptr<DBus::Dispatcher> dispatcher = DBus::StandaloneDispatcher::create();
 	//Create a connection to the session bus
-	DBus::Connection::pointer connection = dispatcher->create_connection( DBus::BUS_SESSION );
+	std::shared_ptr<DBus::Connection> connection = dispatcher->create_connection( DBus::BusType::SESSION );
 	
 	//Request a unique name on the session bus, check for an error.
-	int ret = connection->request_name( "com.rm5248", DBUS_NAME_FLAG_REPLACE_EXISTING );
-  	if (DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER != ret) return 1;
+	DBus::RequestNameResponse ret = connection->request_name( "com.rm5248", DBUSCXX_NAME_FLAG_REPLACE_EXISTING );
+  	if (DBus::RequestNameResponse::PrimaryOwner != ret) return 1;
 
 	//Make a signal proxy which takes in an int, a string, and another int.
-	DBus::signal_proxy<void, int, string, int >::pointer signal = connection->
-                create_signal_proxy<void, int, string, int >
-                ("/", "com.rm5248.SignalInterface", "GenericSignal");
+	DBus::SignalMatchRule rule =
+		DBus::MatchRuleBuilder::create()
+		.set_path( "/" )
+		.set_interface( "com.rm5248.SignalInterface" )
+		.set_member( "GenericSignal" )
+		.as_signal_match();
+	std::shared_ptr<DBus::SignalProxy<void(int,std::string,int)>> signal = connection->
+                create_free_signal_proxy<void(int,std::string,int)>(rule);
                 
     signal->connect( sigc::ptr_fun(getSignal) );
 
 	while( true ){
-		sleep(300);
+		std::this_thread::sleep_for( std::chrono::seconds( 300 ) );
 	}
   	
   	return 0;
